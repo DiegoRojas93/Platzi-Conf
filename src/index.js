4dc4d9179e68b02b330d0b726b0df663bd23de3e
Diego Rojas <diegorojas431@gmail.com>
2020-06-13 13:17:23 -0500

    Sintaxis: JSX y create.Element

--- src/index.js
+++ src/index.js
@@ -1,37 +1,51 @@
-// 👋Hola chicos, **este es el resumen de la clase.**
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// Generalmente para poder maquetar nuestra pagina web por medio de Js vanilla tiende hacer tedioso a medida que crece el proyecto.
+const container = document.getElementById('app');
 
-// **_Ejemplo:_**
+const expresión = 'soy una expresion, lo que quiere decir que soy una algo que se va a evaluar por parte de JavaScript como una operacion (suma, resta, etc) o una funcion, etc.';
 
-// ```
-// const element = document.createElement('h1'); //Crea la etiqueta H1
+/*---------------------React.createElement---------------------*/
 
-// element.innerText = 'Hello, Platzi Badges!'; // Crea y guarda el conteniido de la etiqueta h1
+// Esta sintaxis no es la mas popular de para escribir nuestro codigo pero hace lo mismo que JSX //
 
-// const container = document.getElementById('app'); //Obtenemos el id de la etiqueta div del archivo public=>index.HTML
+// const elemento = React.createElement(
+//   'div',                 // Etiqueta
+//   {},                    // priopiedad de la Etiqueta
+//   'Soy su contenido'     // Contenido de la etiqueta
+// )
 
-// container.appendChild(element); // Agrega el h1 (con todo su contenido) internamente del la etiqueta div del archivo index.HTML
+// const createElement = React.createElement(
+//   'div',
+//   {},
+//   React.createElement(
+//     'h1',
+//     {},
+//     'Hola! soy Diego'
+//   ),
+//   React.createElement(
+//     'a',
+//     { href:'https://Platzi.com' },
+//     'Hola soy un link'
+//   ),
+//   React.createElement(
+//     'P',
+//     {},
+//     `Hola!, ${expresión}`
+//   )
+// )
 
-// /* appendChild: Agrega un nuevo nodo al final de la lista de un elemento hijo de un elemento padre especificado, es dicir, que si un elemento padre (el container es esta ocacion que hace regerecia al div) se le puede agregar un nuevo contenido (el element)
+// ReactDom.render(createElement, container);
 
-// Nota: si el div tiene mas conetido(más etiquetas o estructuras intenas), al hacer ejecutado appendChild, se agregara el elemento al final de este contenido*/
-// ```
+/*-----------------------------JSX------------------------------*/
 
-// Gracias a React podemos mejorar nuestra forma de escribir el codigo de una forma más prolija.
+// Esta es la sintaxis que vamos a utilizar y la que propone React para la creacion de nuestro codigo //
 
-// **_Ejemplo:_**
-
-
-import React from 'react'; //Permitira escribir nuestro codigo con esta nueva sintaxis que vamos a aprender (JSX)
-import ReactDOM from 'react-dom'; // Permite traer la funcionalidad de renderizar (Dibujar en el DOM) el codigo escrito (JSX)
-
-const element = <h1>Hello, Platzi Badges!</h1>; // Sintaxis JSX
-
-const container = document.getElementById('app'); // Obtenemos el id de la etiqueta div del archivo public=>index.HTML
-
-
-
-// ReactDOM.render(__¿Qué vamos a renderizar?__, __¿En dónde lo vamos hacer?__);
-
-ReactDOM.render(element, container); // Funcion que renderizara el codigo
+const jsx =(
+  <div>
+    <h1>Hola Soy Diego</h1>
+    <a href='https://Platzi.com'>Hola soy un link</a>
+    <p>Hola!, {expresión}</p>
+  </div>
+)
+ReactDom.render(jsx, container)
