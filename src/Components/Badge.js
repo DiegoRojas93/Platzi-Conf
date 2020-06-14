342091644f5ca29904a415af8e279806cffd0486
Diego Rojas <diegorojas431@gmail.com>
2020-06-14 08:42:15 -0500

    Cómo aplicar estilos

--- src/Components/Badge.js
+++ src/Components/Badge.js
@@ -2,34 +2,31 @@ import React from 'react'
 
 import confLogo from '../images/badge-header.svg'
 
-// class Badge extends React.Component{
-//   render(){
-//     return(
-//       <h1>soy un componente</h1>
-//     )
-//   }
-// }
+import './styles/Badge.css'
 
 class Badge extends React.Component{
   render(){
     return(
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
-
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+            alt="Avatar"
+          />
           <h1>Diego<br/>Rojas</h1>
         </div>
 
-        <div>
+        <div className="Badge__section-info">
           <p>Frontend Enginner</p>
           <p>@Mordelon4304</p>
         </div>
 
-        <div>
+        <div className="Badge__footer">
           #PlatziConf
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import './global.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
 import Badge from './Components/Badge'
 
 const container = document.getElementById('app');
